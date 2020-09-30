fa1106711029a1547e0a9ee1f1068f35fffc0d39
Nitesh Bhandari <nitesh.bhandari@gmail.com>
2020-09-30 17:08:40 +0530

    - settings.json to use tests venv instead of lib venv - main.py: removed reference to git   - actually this file is good for any config file.   - Need to remove from here and move to tests folder

--- tests/test-git-aliases/main.py
+++ tests/test-git-aliases/main.py
@@ -1,7 +1,7 @@
 from vyAliasBatchScriptGenerator import vyAliasBatchScriptGenerator
 import argparse
 
-parser = argparse.ArgumentParser(description='Generate an alias Windows batch file for git')
+parser = argparse.ArgumentParser(description='Generate an alias Windows batch file')
 parser.add_argument('ConfigFilePath', help='Path to the input config file')
 parser.add_argument('-o', help='Path to the output folder', default='.', dest='OutputFolder', metavar='OutputFolder')
 parser.add_argument('-f', help='Output Filename. NOT RECOMMENDED. Script auto-generates it', default=None, dest='OutputFileName', metavar='OutputFileName')
