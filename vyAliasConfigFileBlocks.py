f2bf88b2884b86ec510bd5b53e845d60768503b0
Nitesh Bhandari <nitesh.bhandari@gmail.com>
2020-10-07 18:58:54 +0530

    - code-workspace renamed to something short and simple - attr:val expressions now as tuples   - more robust and expandable than strings

--- vyAliasConfigFileBlocks.py
+++ vyAliasConfigFileBlocks.py
@@ -4,54 +4,31 @@ class VyAliasesBlock(VyConfigFileBlock):
     pass
 
 VyAliasesBlock.indentLevelMarkers = {
-    0: {
-        None: { 'target': 'aliases', } 
-    },
-    1: {
-                None: { 'target': 'commands', 'mode': 'append',},
-          'label:.*': {},
-        'snippet:.*': {},
-    },
-    2: [VyAliasesBlock, ],
+        0: { None                               : { 'target': 'aliases', } },
+        1: { None                               : { 'target': 'commands', 'mode': 'append',},
+             ('(label|snippet)','.*')           : {},},
+        2:   [VyAliasesBlock, ],
 }
 
 class VyAliasConfigBlock(VyConfigFileBlock):
     indentLevelMarkers = {
-        0: {
-            'vyalias:config': {
-                'target': None
-            },
-        },
-        1: {
-            '.*:.*': {},
-        }
+        0: { ('vyalias', 'config')              : { 'target': None },},
+        1: { ('.*', '.*')                       : {},}
     }
 
 class VyAliasEnvVarBlock(VyConfigFileBlock):
     indentLevelMarkers = {
-        0: {
-            None: {
-                'target': 'envVar',
-            }
-        },
-        1: {
-            'default:.*': {},
-            'target:.*': {},
-            'Target:.*': {},
-        }
+        0: { None                               : { 'target': 'envVar',} },
+        1: { ('(default|[tT]arget)', '.*')      : {}, }
     }
 
 class VyAliasEnvVarHeaderBlock(VyConfigFileBlock):
     indentLevelMarkers = {
-        0: {
-            'vyalias:envvar': {
-                'target': None
-            },
-        },
-        1: [VyAliasEnvVarBlock,],
+        0: { ('vyalias', 'envvar')               : { 'target': None },},
+        1:   [VyAliasEnvVarBlock,],
     }
 
 class VyAliasConfigFileBlock(VyConfigFileBlock):
     indentLevelMarkers = {
-        0: [VyAliasesBlock, VyAliasEnvVarHeaderBlock, VyAliasConfigBlock],
+        0:   [VyAliasesBlock, VyAliasEnvVarHeaderBlock, VyAliasConfigBlock],
     }
