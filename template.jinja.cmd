26709ec59444ab66e90af8c245c4bdd459891e88
Nitesh Bhandari <nitesh.bhandari@gmail.com>
2020-10-09 20:29:08 +0530

    - added jinja2 to project   - output rendering though jinja2 now - cleanup of some redundant files

---
+++ template.jinja.cmd
@@ -0,0 +1,56 @@
+@echo off
+if x%VY_ALIAS_BATCH_GEN_NO_DEBUG%==xtransfer GOTO label_transfer_start
+
+REM '1st call' mode
+set VY_ALIAS_BATCH_GEN_NO_DEBUG=y
+if NOT x%1==xdebug GOTO label_start
+
+REM 'debug' mode
+echo Debug mode on
+set VY_ALIAS_BATCH_GEN_NO_DEBUG=transfer
+%0 %2 %3 %4 %5 %6 %7 %8 %9
+GOTO label_exit
+
+REM 'transfer' mode. Here we debug by setting VY_ALIAS_BATCH_GEN_NO_DEBUG=<blank>
+:label_transfer_start
+echo Tranferred into batchfile sub call
+set VY_ALIAS_BATCH_GEN_NO_DEBUG=
+if x%VY_ALIAS_BATCH_GEN_NO_DEBUG%==x echo arguments    :: (0, '%0') (1, '%1') (2, '%2') (3, '%3') (4, '%4') (5, '%5') (6, '%6') (7, '%7') (8, '%8') (9, '%9')
+if x%VY_ALIAS_BATCH_GEN_NO_DEBUG%==x echo command line :: %0 %1 %2 %3 %4 %5 %6 %7 %8 %9
+echo ##############################################################
+GOTO label_start
+
+##############################################################
+REM Actual start of the program
+:label_start
+{% for envvar in envvars %}if x%VY_GIT_CMD_{{ envvar["envVar"] }}%==x SET VY_GIT_CMD_{{ envvar["envVar"] }}={{ envvar["default"] }}
+{% endfor %}{% for aliasObj in aliasQueue %}{% if aliasObj.firstchild %}
+:label_{{ aliasObj.parent.final.label }}
+if x%VY_ALIAS_BATCH_GEN_NO_DEBUG%==x echo label_{{ aliasObj.parent.final.label }} & REM ({{ aliasObj.parent.final.label }}) <- ({{ aliasObj.final.label }}){% endif %}{% for alias in aliasObj.aliases %}
+if x%{{ aliasObj.level }}==x{{ '{0:<9}'.format(alias) }} GOTO label_{{ aliasObj.final.label }}{% endfor %}{% if aliasObj.lastchild %}
+GOTO label_invalid & REM ({{ aliasObj.parent.final.label }}) <- ({{ aliasObj.final.label }}){% endif %}{% endfor %}
+
+##############################################################
+:label_invalid
+echo Invalid command. Type '%0 h' to get help
+GOTO label_exit
+
+##############################################################
+:label_{{ labelHelp }}
+@echo on
+@echo Command Options:{% for aliasObj in tree.root.traverse() %}{% if aliasObj.traversalState == 'pre' and not aliasObj.hasChildren %}
+@echo   {{ '{0:<11}'.format(aliasObj.final.primaryAlias) }}   : {{ aliasObj.final.snippet }}{% endif %}{% endfor %}
+@echo.{% for envvar in envvars %}
+@echo Set environment variable 'VY_GIT_CMD_{{ envvar["envVar"] }}' to set {{ envvar["target"] }}. default='{{ envvar["default"] }}', current='%VY_GIT_CMD_{{ envvar["envVar"] }}%'{% endfor %}
+@echo off
+GOTO label_exit
+{% for aliasObj in tree.root.traverse() %}{% if aliasObj.traversalState == 'pre' and not aliasObj.hasChildren and (aliasObj.commands)|length > 0 %}
+##############################################################
+:label_{{ aliasObj.final.label }}
+@echo on
+{{ aliasObj.commandsStr }}
+@echo off
+GOTO label_exit
+{% endif %}{% endfor %}
+##############################################################
+:label_exit
--- vyAliasBatchScriptGenerator.py
+++ vyAliasBatchScriptGenerator.py
@@ -1,6 +1,5 @@
 import os
-from vyImport import vyLoadModuleFromFilePath
-from .vyAliasBlock import VyAliasBlock
+from jinja2 import Template
 from .vyAliasTree import VyAliasTree
 from .vyAliasConfigFile import VyAliasConfigFile
 
@@ -12,71 +11,23 @@ class VyCOIdx():
 def vyAliasBatchScriptGenerator(configFilePath, outputFolder='.', outputFileName=None):
     acf = VyAliasConfigFile(configFilePath)
     aliasRootBlock, envVarInfos, configInfos = acf.parse()
-
-    cmdTemplates = None
-    envVarTemplates = None
-    moduleFolder = os.path.dirname(os.path.realpath(__file__))
-    subTemplates = vyLoadModuleFromFilePath(os.path.join(moduleFolder, 'subTemplates.py'))
-    cmdTemplates = subTemplates.cmdTemplates
-    envVarTemplates = subTemplates.envVarTemplates
-
     tree = VyAliasTree(aliasRootBlock, **configInfos)
     tree.root.final.label = 'Switcher'
 
-    envVarOutputs = [''] * len(envVarTemplates)
-    cmdOutputs = [''] * len(cmdTemplates) + [''] + ['']
-
     aliasQueue = [_ for _ in tree.root.subAliasBlocks]
     for aliasObj in aliasQueue:
-        if aliasObj.firstchild:
-            cmdOutputs[VyCOIdx.Switcher] += cmdTemplates[3].format(
-                parent_label=aliasObj.parent.final.label,
-                label=aliasObj.final.label)
-
-        for alias in aliasObj.aliases:
-            cmdOutputs[VyCOIdx.Switcher] += cmdTemplates[0].format(
-                level=aliasObj.level,
-                alias=alias,
-                label=aliasObj.final.label,
-            )
-
-        if aliasObj.lastchild:
-            cmdOutputs[VyCOIdx.Switcher] += f'GOTO label_invalid & REM ({aliasObj.parent.final.label}) <- ({aliasObj.final.label})\n'
-
         if aliasObj.hasChildren:
             aliasQueue += [_ for _ in aliasObj.subAliasBlocks]
-
-    for aliasObj in tree.root.traverse():
-        if aliasObj.traversalState == 'post':
-            continue
-        if not aliasObj.hasChildren:
-            cmdOutputs[VyCOIdx.HelpSnippets] += cmdTemplates[1].format(
-                alias=aliasObj.final.primaryAlias,
-                final_snippet=aliasObj.final.snippet,
-            )
-            if len(aliasObj.commands) > 0:
-                cmdOutputs[VyCOIdx.Commands] += cmdTemplates[2].format(
-                    commands=aliasObj.commandsStr,
-                    label=aliasObj.final.label,
-                )
-
-    for envVarInfo in envVarInfos:
-        suffix = envVarInfo['envVar']
-        default = envVarInfo['default']
-        target = envVarInfo['target'] if 'target' in envVarInfo else suffix.lower()
-        Target = envVarInfo['Target'] if 'Target' in envVarInfo else target[0].upper() + target[1:]
-        for idx, envVarTemplate in enumerate(envVarTemplates):
-            envVarOutputs[idx] += envVarTemplate.format(
-                suffix=suffix,
-                default=default,
-                target=target,
-                Target=Target,
-            )
-    # procesing done
-    gTemplate = open(os.path.join(moduleFolder, 'template.cmd')).read()
-    gTemplate = gTemplate.format(VyCOIdx=VyCOIdx)
     labelHelp = tree.root.subAliasBlocks[0].final.label # help is inserted in the beginning
-    out = gTemplate.format(ev=envVarOutputs, cmd=cmdOutputs, labelHelp=labelHelp)
+    for envVarInfo in envVarInfos:
+        envVarInfo['target'] = envVarInfo['target'] if 'target' in envVarInfo else envVarInfo['envVar'].lower()
+        envVarInfo['Target'] = envVarInfo['Target'] if 'Target' in envVarInfo else envVarInfo['target'][0].upper() + envVarInfo['target'][1:]
+
+    # Jinja Starts Here
+    moduleFolder = os.path.dirname(os.path.realpath(__file__))
+    template = Template(open(os.path.join(moduleFolder, 'template.jinja.cmd')).read())
+    out = template.render(labelHelp=labelHelp, envvars=envVarInfos, tree=tree, aliasQueue=aliasQueue)
+
     if not outputFileName:
         outputFileName = f'{tree.root.aliases[0]}.cmd'
     outputFilePath = os.path.join(outputFolder, outputFileName)
