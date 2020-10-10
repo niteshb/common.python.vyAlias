36c86a9ceb5a94dcab0d807ba98133541755aa8e
Nitesh Bhandari <nitesh.bhandari@gmail.com>
2020-10-10 07:09:13 +0530

    - template changed with whitespace management

--- template.jinja.cmd
+++ template.jinja.cmd
@@ -23,12 +23,21 @@ GOTO label_start
 ##############################################################
 REM Actual start of the program
 :label_start
-{% for envvar in envvars %}if x%VY_GIT_CMD_{{ envvar["envVar"] }}%==x SET VY_GIT_CMD_{{ envvar["envVar"] }}={{ envvar["default"] }}
-{% endfor %}{% for aliasObj in aliasQueue %}{% if aliasObj.firstchild %}
+{%- for envvar in envvars %}
+if x%VY_GIT_CMD_{{ envvar["envVar"] }}%==x SET VY_GIT_CMD_{{ envvar["envVar"] }}={{ envvar["default"] }}
+{%- endfor %}
+{% for aliasObj in aliasQueue %}
+{%-     if aliasObj.firstchild %}
 :label_{{ aliasObj.parent.final.label }}
-if x%VY_ALIAS_BATCH_GEN_NO_DEBUG%==x echo label_{{ aliasObj.parent.final.label }} & REM ({{ aliasObj.parent.final.label }}) <- ({{ aliasObj.final.label }}){% endif %}{% for alias in aliasObj.aliases %}
-if x%{{ aliasObj.level }}==x{{ '{0:<9}'.format(alias) }} GOTO label_{{ aliasObj.final.label }}{% endfor %}{% if aliasObj.lastchild %}
-GOTO label_invalid & REM ({{ aliasObj.parent.final.label }}) <- ({{ aliasObj.final.label }}){% endif %}{% endfor %}
+if x%VY_ALIAS_BATCH_GEN_NO_DEBUG%==x echo label_{{ aliasObj.parent.final.label }} & REM ({{ aliasObj.parent.final.label }}) <- ({{ aliasObj.final.label }})
+{%-     endif %}
+{%-     for alias in aliasObj.aliases %}
+if x%{{ aliasObj.level }}==x{{ '{0:<9}'.format(alias) }} GOTO label_{{ aliasObj.final.label }}
+{%-     endfor %}
+{%-     if aliasObj.lastchild %}
+GOTO label_invalid & REM ({{ aliasObj.parent.final.label }}) <- ({{ aliasObj.final.label }})
+{%-     endif %}
+{%- endfor %}
 
 ##############################################################
 :label_invalid
@@ -38,19 +47,27 @@ GOTO label_exit
 ##############################################################
 :label_{{ labelHelp }}
 @echo on
-@echo Command Options:{% for aliasObj in aliasRootBlock.traverse() %}{% if aliasObj.traversalState == 'pre' and not aliasObj.hasChildren %}
-@echo   {{ '{0:<11}'.format(aliasObj.final.primaryAlias) }}   : {{ aliasObj.final.snippet }}{% endif %}{% endfor %}
-@echo.{% for envvar in envvars %}
-@echo Set environment variable 'VY_GIT_CMD_{{ envvar["envVar"] }}' to set {{ envvar["target"] }}. default='{{ envvar["default"] }}', current='%VY_GIT_CMD_{{ envvar["envVar"] }}%'{% endfor %}
+@echo Command Options:
+{%- for aliasObj in aliasRootBlock.traverse() %}
+{%-     if aliasObj.traversalState == 'pre' and not aliasObj.hasChildren %}
+@echo   {{ '{0:<11}'.format(aliasObj.final.primaryAlias) }}   : {{ aliasObj.final.snippet }}
+{%-     endif %}
+{%- endfor %}
+@echo.
+{%- for envvar in envvars %}
+@echo Set environment variable 'VY_GIT_CMD_{{ envvar["envVar"] }}' to set {{ envvar["target"] }}. default='{{ envvar["default"] }}', current='%VY_GIT_CMD_{{ envvar["envVar"] }}%'
+{%- endfor %}
 @echo off
 GOTO label_exit
-{% for aliasObj in aliasRootBlock.traverse() %}{% if aliasObj.traversalState == 'pre' and not aliasObj.hasChildren and (aliasObj.commands)|length > 0 %}
+{% for aliasObj in aliasRootBlock.traverse() %}
+{%-    if aliasObj.traversalState == 'pre' and not aliasObj.hasChildren and (aliasObj.commands)|length > 0 %}
 ##############################################################
 :label_{{ aliasObj.final.label }}
 @echo on
 {{ aliasObj.commandsStr }}
 @echo off
 GOTO label_exit
-{% endif %}{% endfor %}
+{%     endif %}
+{%- endfor %}
 ##############################################################
 :label_exit
